0ad763a53b155e343ec098786268b5ec74e4f8bc
yeissonleandrocruzrangel <playgaeton@gmail.com>
2017-10-19 10:10:48 -0500

    Se creo ng g component customers/index

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AddComponent, ListComponent]
+  declarations: [AddComponent, ListComponent, IndexComponent]
 })
 export class CustomersModule { }
