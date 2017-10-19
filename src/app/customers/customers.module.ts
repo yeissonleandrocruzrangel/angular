d6b99efc4e9476de5cf3799782a309bb8434fb84
yeissonleandrocruzrangel <playgaeton@gmail.com>
2017-10-19 10:08:29 -0500

    Se crearon ng g component customers/add y ng g component customers/list

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [AddComponent, ListComponent]
 })
 export class CustomersModule { }
